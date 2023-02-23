5edf25676ee47f06c311ec008a3ef354485ecc2f
fabio-marcelo <fabiomarcelomedvet@gmail.com>
2023-02-23 17:48:28 +0000

    add link to download ref sh_qiime_release_s_29.11.2022

--- get_ref.sh
+++ get_ref.sh
@@ -5,6 +5,11 @@ tar xzf 98AE96C6593FC9C52D1C46B96C2D9064291F4DBA625EF189FEC1CCAFCF4A1691.gz
 
 cd sh_qiime_release_04.02.2020/developer/
 
-awk '/^>/ {print($0)}; /^[^>]/ {print(toupper($0))}' sh_refs_qiime_ver8_99_04.02.2020_dev.fasta | tr -d ' ' > sh_refs_qiime_ver8_99_04.02.2020_dev_uppercase.fasta
 
 
+# download da versao sh_qiime_release_s_29.11.2022.tgz
+wget https://files.plutof.ut.ee/public/orig/98/AE/671C4D441E50DCD30691B84EED22065D77BAD3D18AF1905675633979BF323754.tgz
+
+
+awk '/^>/ {print($0)}; /^[^>]/ {print(toupper($0))}' sh_refs_qiime_ver8_99_04.02.2020_dev.fasta \
+| tr -d ' ' > sh_refs_qiime_ver8_99_04.02.2020_dev_uppercase.fasta

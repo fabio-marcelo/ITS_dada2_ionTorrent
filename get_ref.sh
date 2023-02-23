b363ad790e2b9391c5910d08143cdabde6d2d3a0
fabio-marcelo <fabiomarcelomedvet@gmail.com>
2023-02-23 17:43:44 +0000

    tt

--- get_ref.sh
+++ get_ref.sh
@@ -1,3 +1,4 @@
+# download de db unite
 wget https://files.plutof.ut.ee/public/orig/98/AE/98AE96C6593FC9C52D1C46B96C2D9064291F4DBA625EF189FEC1CCAFCF4A1691.gz
 
 tar xzf 98AE96C6593FC9C52D1C46B96C2D9064291F4DBA625EF189FEC1CCAFCF4A1691.gz
@@ -5,3 +6,5 @@ tar xzf 98AE96C6593FC9C52D1C46B96C2D9064291F4DBA625EF189FEC1CCAFCF4A1691.gz
 cd sh_qiime_release_04.02.2020/developer/
 
 awk '/^>/ {print($0)}; /^[^>]/ {print(toupper($0))}' sh_refs_qiime_ver8_99_04.02.2020_dev.fasta | tr -d ' ' > sh_refs_qiime_ver8_99_04.02.2020_dev_uppercase.fasta
+
+

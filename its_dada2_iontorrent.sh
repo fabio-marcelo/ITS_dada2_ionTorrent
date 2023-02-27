8cbfe6a96066ae8709ba5426cc8547c52bd9d978
fabio-marcelo <fabiomarcelomedvet@gmail.com>
2023-02-27 20:10:30 +0000

    shell

--- its_dada2_iontorrent.sh
+++ its_dada2_iontorrent.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Immediately stop on errors. keep track of the commands as they get executed. 
+set -uex
+
 # This bash file runs analysis for fungal ITS sequencing data generated in Ion S platform
 # through qiime2 using dada2
 
+
+ls

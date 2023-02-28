feca02499cad2b5c554c4ef312a9ce086a19170f
fabio-marcelo <fabiomarcelomedvet@gmail.com>
2023-02-28 12:19:47 +0000

    add command line flags

--- its_dada2_iontorrent.sh
+++ its_dada2_iontorrent.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/env bash
 
 # Immediately stop on errors. keep track of the commands as they get executed. 
 set -uex
@@ -7,76 +7,84 @@ set -uex
 # through qiime2 using dada2
 
 #define fastq files folder
-folder=data_toy
-primer=
-reference_folder=
+while getopts i:p:r: flag
+do
+    case "${flag}" in
+        i) fastq_folder=${OPTARG};;
+        p) primer_seq=${OPTARG};;
+        r) ref_folder=${OPTARG};;
+    esac
+done
+echo "fastq_folder: $fastq_folder";
+echo "primer_seq: $primer_seq";
+echo "ref_folder: $ref_folder";
+
 
 # create manifest file
-echo "sample-id" > sample-id.txt
-echo "absolute-filepath" > filepath.txt
+echo "sample-id" > "$fastq_folder"/temp/sample-id.txt
+echo "absolute-filepath" > "$fastq_folder"/temp/filepath.txt
 
-for i in $(ls "$folder"); do echo "$i" | awk -F _bp_ '{print $2}' | awk -F . '{print $1}' >> sample-id.txt; done
+for i in $(ls "$fastq_folder"); do echo "$i" | awk -F _bp_ '{print $2}' | awk -F . '{print $1}' >> "$fastq_folder"/temp/sample-id.txt; done
 
-find $(pwd)/"$folder"/*fastq >> filepath.txt
+find $(pwd)/"$fastq_folder"/*fastq >> "$fastq_folder"/temp/filepath.txt
 
-paste sample-id.txt filepath.txt > manifest-file.tsv
+paste "$fastq_folder"/temp/sample-id.txt "$fastq_folder"/temp/filepath.txt > "$fastq_folder"/temp/manifest-file.tsv
 
 # import fastq files
 qiime tools import --type 'SampleData[SequencesWithQuality]' \       
-  --input-path manifest-file.tsv \                              
-  --output-path fastq_imported.qza \                                         
+  --input-path "$fastq_folder"/temp/manifest-file.tsv \                              
+  --output-path "$fastq_folder"/temp/fastq_imported.qza \                                         
   --input-format SingleEndFastqManifestPhred33V2  
 
 # visualize fastq files imported
 qiime demux summarize \
---i-data import.qza \
---o-visualization inspec_import.qzv 
+--i-data "$fastq_folder"/temp/fastq_imported.qza \
+--o-visualization "$fastq_folder"/output/inspec_import.qzv 
 
 # trim primers
 echo "start trimming"
 qiime cutadapt trim-single \
---i-demultiplexed-sequences fastq_imported.qza \
---p-front GGAAGTAAAAGTCGTAACAAGG \
+--i-demultiplexed-sequences "$fastq_folder"/temp/fastq_imported.qza \
+--p-front "$primer_seq" \
 --p-error-rate 0 \
---o-trimmed-sequences trimmed-seqs.qza \
+--o-trimmed-sequences "$fastq_folder"/temp/trimmed-seqs.qza \
 --verbose
 
 # denoising
 echo "start denoising"
 qiime dada2 denoise-single \
---i-demultiplexed-seqs trimmed-seqs.qza \
+--i-demultiplexed-seqs "$fastq_folder"/temp/trimmed-seqs.qza \
 --p-max-ee 2 \
 --p-trunc-q 2 \
 --p-trunc-len 0 \
 --p-pooling-method 'pseudo' \
 --p-chimera-method 'consensus' \
---o-representative-sequences representative-seqs.qza \
---o-table table-denoised.qza \
---o-denoising-stats denoise-stats.qza
+--o-representative-sequences "$fastq_folder"/temp/representative-seqs.qza \
+--o-table "$fastq_folder"/temp/table-denoised.qza \
+--o-denoising-stats "$fastq_folder"/temp/denoise-stats.qza
 
 # visualize denoising
 qiime metadata tabulate \
---m-input-file denoise-stats.qza \
---o-visualization inspect_denoise-stats.qzv
+--m-input-file "$fastq_folder"/temp/denoise-stats.qza \
+--o-visualization "$fastq_folder"/output/inspect_denoise-stats.qzv
 echo "open qzv file in https://view.qiime2.org/"
 
 # train classifier
 echo "Import seq reference file"
-
 qiime tools import \
 --type FeatureData[Sequence] \
---input-path reference_folder/sh_refs_qiime_ver9_99_s_29.11.2022_dev.fasta \
---output-path reference_sequences.qza
+--input-path "$ref_folder"/sh_refs_qiime_ver9_99_s_29.11.2022_dev.fasta \
+--output-path "$fastq_folder"/temp/reference_sequences.qza
 
 echo "Import taxonomy reference file"
 qiime tools import \
 --type FeatureData[Taxonomy] \
---input-path reference_folder/sh_taxonomy_qiime_ver9_99_s_29.11.2022_dev.txt \
---output-path reference_taxonomy.qza \
+--input-path "$ref_folder"/sh_taxonomy_qiime_ver9_99_s_29.11.2022_dev.txt \
+--output-path "$fastq_folder"/temp/reference_taxonomy.qza \
 --input-format HeaderlessTSVTaxonomyFormat
 
 echo "Start trainning classifier"
 qiime feature-classifier fit-classifier-naive-bayes \
---i-reference-reads reference_sequences.qza \
---i-reference-taxonomy reference_taxonomy.qza \
---o-classifier trainned_classifier_qiime_release_s_29.11.2022.qza  
+--i-reference-reads "$fastq_folder"/temp/reference_sequences.qza \
+--i-reference-taxonomy "$fastq_folder"/temp/reference_taxonomy.qza \
+--o-classifier "$fastq_folder"/temp/trainned_classifier_qiime_release_s_29.11.2022.qza  
